99d109e806a5cc3b21f6fadca748ab8af099c3e3
Jared Tyler <jared@mappedin.ca>
2019-05-13 02:19:33 -0400

    Parse DB connection details from Heroku-friendly env var.

--- db/Resource.go
+++ db/Resource.go
@@ -3,6 +3,9 @@ package db
 import (
 	"fmt"
 	"github.com/go-pg/pg"
+	"net/url"
+	"os"
+	"strings"
 )
 
 type dbLogger struct{}
@@ -21,10 +24,33 @@ func InitDatabase() *pg.DB {
 		return DB
 	}
 
-	DB = pg.Connect(&pg.Options{
-		User:     "postgres",
-		Password: "postgres",
-	})
+	databaseUrl := os.Getenv("DATABASE_URL")
+	if databaseUrl == "" {
+		// TODO: throw error, need database
+	}
+
+	auth, err := url.Parse(databaseUrl)
+	if err != nil {
+		panic(err)
+	}
+
+	if auth.Scheme != "postgres" {
+		// TODO: throw error, DB must be postgres
+	}
+
+	password, _ := auth.User.Password()
+	database := strings.TrimPrefix(auth.Path, "/")
+
+	connOptions := &pg.Options{
+		User:     auth.User.Username(),
+		Password: password,
+		Addr:     auth.Host,
+		Database: database,
+		// ApplicationName: "HomestuckAPI",
+	}
+
+	fmt.Println("Database Connection:", connOptions)
+	DB = pg.Connect(connOptions)
 
 	// DB.AddQueryHook(dbLogger{})
 
