7bdc0dfc242db7f3ef57b637388f04b5f385785f
Jared Tyler <jared@mappedin.ca>
2019-06-10 00:59:19 -0400

    Tinker with in-code cron and keeping app alive.

--- src/clock/main.go
+++ src/clock/main.go
@@ -6,6 +6,10 @@ import (
 	// "github.com/jackc/pgx"
 	"homestuck-watcher/db"
 	// "os"
+	"github.com/robfig/cron"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -13,12 +17,28 @@ func main() {
 	defer fmt.Println("\n[[[WORK COMPLETE]]]")
 	defer db.CloseDatabase()
 
-	// TODO: once every minute, do lightweight
-	// ... start one-off dyno of `clock/worker lightweight`
-	go runLightweightWorker()
+	c := cron.New()
+	c.AddFunc("@every 1m", func() {
+		// TODO: once every minute, do lightweight
+		// ... start one-off dyno of `clock/worker lightweight`
+		go runLightweightWorker()
+	})
+	c.AddFunc("@daily", func() {
+		// TODO: once per day, do heavyweight
+		// ...
+	})
+	c.Start()
+	defer c.Stop()
+	defer fmt.Println("Stopping cron ...")
 
-	// TODO: once per day, do heavyweight
-	// ...
+	// .................
+
+	// Catch signal so we can shutdown gracefully
+	sigCh := make(chan os.Signal)
+	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT)
+
+	// Wait for a signal
+	<-sigCh
 
 	// pgxcfg, err := pgx.ParseURI(os.Getenv("DATABASE_URL"))
 	// if err != nil {
